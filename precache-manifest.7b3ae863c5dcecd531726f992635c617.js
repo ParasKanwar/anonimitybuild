6e4c3c5144ee605e54ff1852f6c5e11fc3703335
paras <paraskanwar30@gmail.com>
2020-04-13 20:19:30 +0530

    added beautyful icons

---
+++ precache-manifest.7b3ae863c5dcecd531726f992635c617.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c9a3efc3b7e3fa13e0e86b2fd5f393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c1143b7b726bb5cbfc5",
+    "url": "/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "ec106733416c6f6d4a30",
+    "url": "/static/css/main.fbf39286.chunk.css"
+  },
+  {
+    "revision": "4c1143b7b726bb5cbfc5",
+    "url": "/static/js/2.40b77d22.chunk.js"
+  },
+  {
+    "revision": "de5933bbfe8f970953aa351dcd16a9a6",
+    "url": "/static/js/2.40b77d22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec106733416c6f6d4a30",
+    "url": "/static/js/main.1e688d82.chunk.js"
+  },
+  {
+    "revision": "d05a8293104dc26ae6c5",
+    "url": "/static/js/runtime-main.18057c50.js"
+  },
+  {
+    "revision": "bc7f0fe661388010b3c9c80d282649d6",
+    "url": "/static/media/complainify.bc7f0fe6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9cfa3cccaa5f274482cf77c8f4ace54.js"
+  "/precache-manifest.7b3ae863c5dcecd531726f992635c617.js"
 );
 
 self.addEventListener('message', (event) => {
