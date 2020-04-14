d180127dae8dfb129926cce22dd7f71b6124c93b
paras <paraskanwar30@gmail.com>
2020-04-14 23:21:49 +0530

    added About

---
+++ precache-manifest.573c686a28fd0ea254bab01cbc8c39a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef7496b44044b401a4f902a7dc92320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd17b99c7ce33e361b3b",
+    "url": "/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "3e90df4198bf9d0ffd7c",
+    "url": "/static/css/main.16a3c7ab.chunk.css"
+  },
+  {
+    "revision": "dd17b99c7ce33e361b3b",
+    "url": "/static/js/2.dcb02bf5.chunk.js"
+  },
+  {
+    "revision": "de5933bbfe8f970953aa351dcd16a9a6",
+    "url": "/static/js/2.dcb02bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e90df4198bf9d0ffd7c",
+    "url": "/static/js/main.7a4c08f3.chunk.js"
+  },
+  {
+    "revision": "d05a8293104dc26ae6c5",
+    "url": "/static/js/runtime-main.18057c50.js"
+  },
+  {
+    "revision": "bc7f0fe661388010b3c9c80d282649d6",
+    "url": "/static/media/complainify.bc7f0fe6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40d9050111ce3529dffb04279fdbcc49.js"
+  "/precache-manifest.573c686a28fd0ea254bab01cbc8c39a5.js"
 );
 
 self.addEventListener('message', (event) => {
